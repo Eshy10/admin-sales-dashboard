6cb5901cb734067e1e6cbc6ce32904028b3b09e0
Eshy10 <adewalebose15@gmail.com>
2024-03-07 02:32:37 +0100

    chore: remove unused files

--- src/components/home/index.ts
+++
@@ -1,5 +0,0 @@
-export { default as HomeHeader } from './header';
-export { default as FindArtiste } from './find';
-export { default as JoinArtiste } from './joinArtiste';
-export { default as ArtisteStats } from './stats';
-export { default as ArtistCareer } from './career';
